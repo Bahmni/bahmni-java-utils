7c43854718252cec6e14d55ff9a752155cb23257
arathyjan <arathyjanardhanan@gmail.com>
2013-12-23 18:24:51 +0530

    RT,Indraneel | #1558 | reverting to older version

--- web-clients/src/main/java/org/bahmni/webclients/openmrs/OpenMRSLoginAuthenticator.java
+++ web-clients/src/main/java/org/bahmni/webclients/openmrs/OpenMRSLoginAuthenticator.java
@@ -6,25 +6,19 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.CoreConnectionPNames;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.bahmni.webclients.*;
 import org.codehaus.jackson.map.ObjectMapper;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 
-public class OpenMRSLoginAuthenticator  implements Authenticator {
+public class OpenMRSLoginAuthenticator implements Authenticator {
     private static Logger logger = Logger.getLogger(OpenMRSLoginAuthenticator.class);
     private final String SESSION_ID_KEY = "JSESSIONID";
 
@@ -46,24 +40,33 @@ public class OpenMRSLoginAuthenticator  implements Authenticator {
 
     @Override
     public HttpRequestDetails refreshRequestDetails(URI uri) {
+        String responseText = null;
         DefaultHttpClient httpClient = new DefaultHttpClient();
         try {
-            CookieStore cookieStore = new BasicCookieStore();
-            HttpContext httpContext = new BasicHttpContext();
-            httpContext.setAttribute(ClientContext.COOKIE_STORE,cookieStore);
-
             httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, authenticationDetails.getReadTimeout());
             httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, authenticationDetails.getConnectionTimeout());
 
             HttpGet httpGet = new HttpGet(authenticationDetails.getAuthUrl());
+
             setCredentials(httpGet);
 
             logger.info(String.format("Executing request: %s", httpGet.getRequestLine()));
 
-            HttpResponse response = httpClient.execute(httpGet, httpContext);
+            HttpResponse response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                InputStream content = entity.getContent();
+                responseText = IOUtils.toString(content);
+            }
+            logger.info(String.format("Authentication response: %s", responseText));
+            EntityUtils.consume(entity);
+            OpenMRSAuthenticationResponse openMRSResponse = new ObjectMapper().readValue(responseText, OpenMRSAuthenticationResponse.class);
+            confirmAuthenticated(openMRSResponse);
 
+            ClientCookies clientCookies = new ClientCookies();
+            clientCookies.put(SESSION_ID_KEY, openMRSResponse.getSessionId());
 
-            processResponse(uri, response);
+            previousSuccessfulRequest = new HttpRequestDetails(uri, clientCookies, new HttpHeaders());
             return previousSuccessfulRequest;
 
         } catch (Exception e) {
@@ -73,24 +76,7 @@ public class OpenMRSLoginAuthenticator  implements Authenticator {
         }
     }
 
-    private void processResponse(URI uri, HttpResponse response) throws IOException {
-        HttpEntity entity = response.getEntity();
-        String responseText = null;
-        if (entity != null) {
-            InputStream content = entity.getContent();
-            responseText = IOUtils.toString(content);
-        }
-        logger.info(String.format("Authentication response: %s", responseText));
-        EntityUtils.consume(entity);
-        OpenMRSAuthenticationResponse openMRSResponse = new ObjectMapper().readValue(responseText, OpenMRSAuthenticationResponse.class);
-        confirmAuthenticated(openMRSResponse);
-
-        ClientCookies clientCookies = new ClientCookies();
-        clientCookies.put(SESSION_ID_KEY, openMRSResponse.getSessionId());
-        previousSuccessfulRequest = new HttpRequestDetails(uri, clientCookies, new HttpHeaders());
-    }
-
-    private void setCredentials(HttpGet httpGet) throws AuthenticationException {
+    protected void setCredentials(HttpGet httpGet) throws AuthenticationException {
         UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(authenticationDetails.getUserId(), authenticationDetails.getPassword());
         BasicScheme scheme = new BasicScheme();
         Header authorizationHeader = scheme.authenticate(credentials, httpGet);
