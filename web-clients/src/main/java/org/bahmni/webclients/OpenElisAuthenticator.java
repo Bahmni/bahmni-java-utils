4292226feb0f07eb6cdd96b51cf2602f0bd024b4
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-10-13 13:09:27 +0530

    Jaswanth | #2374 | Add openelis authenticator

---
+++ web-clients/src/main/java/org/bahmni/webclients/OpenElisAuthenticator.java
@@ -0,0 +1,84 @@
+package org.bahmni.webclients;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.log4j.Logger;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OpenElisAuthenticator implements Authenticator {
+    private static Logger logger = Logger.getLogger(OpenElisAuthenticator.class);
+    private final ConnectionDetails authenticationDetails;
+    private HttpRequestDetails previousSuccessfulRequest;
+
+    public OpenElisAuthenticator(ConnectionDetails authenticationDetails) {
+        this.authenticationDetails = authenticationDetails;
+    }
+
+    @Override
+    public HttpRequestDetails getRequestDetails(URI uri) {
+        if (previousSuccessfulRequest == null || previousSuccessfulRequest.getClientCookies().size() == 0) {
+            return refreshRequestDetails(uri);
+        }
+        return previousSuccessfulRequest.createNewWith(uri);
+    }
+
+    @Override
+    public HttpRequestDetails refreshRequestDetails(URI uri) {
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        try {
+            CookieStore cookieStore = new BasicCookieStore();
+            HttpContext httpContext = new BasicHttpContext();
+            httpContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+
+            httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, authenticationDetails.getReadTimeout());
+            httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, authenticationDetails.getConnectionTimeout());
+
+            HttpPost httpPost = new HttpPost(uri);
+
+            logger.info(String.format("Executing request: %s", httpPost.getRequestLine()));
+
+            List<NameValuePair> params = new ArrayList<>();
+            params.add(new BasicNameValuePair("loginName", authenticationDetails.getUserId()));
+            params.add(new BasicNameValuePair("password", authenticationDetails.getPassword()));
+            httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+            HttpResponse response = httpClient.execute(httpPost, httpContext);
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new WebClientsException("Authentication with OpenELIS failed");
+            }
+
+            ClientCookies clientCookies = new ClientCookies();
+            List<Cookie> cookies = cookieStore.getCookies();
+            for (Cookie cookie : cookies) {
+                String SESSION_ID_KEY = "JSESSIONID";
+                if (cookie.getName().equalsIgnoreCase(SESSION_ID_KEY)) {
+                    clientCookies.put(SESSION_ID_KEY, cookie.getValue());
+                }
+            }
+
+            previousSuccessfulRequest = new HttpRequestDetails(uri, clientCookies, new HttpHeaders());
+            return previousSuccessfulRequest;
+
+        } catch (Exception e) {
+            throw new WebClientsException(e);
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+
+}
