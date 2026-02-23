de0648790cbce3566342f6b18b6fa13b8ca9bcd5
ravinderkabli <ravinderkabli@users.noreply.github.com>
2026-02-23 22:13:58 +0530

    BAH-4421|Updated file header text

--- form2-utils/src/main/resources/sql/formList.sql
+++ form2-utils/src/main/resources/sql/formList.sql
@@ -1,11 +1,9 @@
 -- /
 -- This Source Code Form is subject to the terms of the Mozilla Public License,
 -- v. 2.0. If a copy of the MPL was not distributed with this file, You can
--- obtain one at http://mozilla.org/MPL/2.0/. Bahmni is also distributed under
--- the terms of the Healthcare Disclaimer located at https://www.bahmni.org/license
--- 
--- Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
--- graphic logo is a trademark of OpenMRS Inc.
+-- obtain one at https://www.bahmni.org/license/mplv2hd.
+--
+-- Copyright (C) 2026 OpenMRS Inc.
 -- /
 
 SELECT
